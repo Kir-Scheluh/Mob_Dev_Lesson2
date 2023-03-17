a352137c206a670bf6797d3eb55521643eaaba88
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-03-17 22:25:09 +0300

    Добавлены собственные диалоговые окна

--- dialog/src/main/java/com/mirea/schelukhinka/dialog/MainActivity.java
+++ dialog/src/main/java/com/mirea/schelukhinka/dialog/MainActivity.java
@@ -2,32 +2,78 @@ package com.mirea.schelukhinka.dialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class MainActivity extends AppCompatActivity {
+    Calendar dateAndTime = Calendar.getInstance();
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
         setContentView(R.layout.activity_main);
     }
+
 
     public void onClickShowDialog(View view) {
         MyDialogFragment dialogFragment = new MyDialogFragment();
         dialogFragment.show(getSupportFragmentManager(), "mirea");
     }
-    public void onOkClicked() {
-        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Иду дальше\"!",
-                Toast.LENGTH_LONG).show();
+        public void onOkClicked() {
+            Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Иду дальше\"!",
+                    Toast.LENGTH_LONG).show();
+        }
+        public void onCancelClicked() {
+            Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Нет\"!",
+                    Toast.LENGTH_LONG).show();
+        }
+        public void onNeutralClicked() {
+            Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"На паузе\"!",
+                    Toast.LENGTH_LONG).show();
+        }
+
+
+    // отображаем диалоговое окно для выбора даты
+    public void setDate(View v) {
+        new DatePickerDialog(MainActivity.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
     }
-    public void onCancelClicked() {
-        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Нет\"!",
-                Toast.LENGTH_LONG).show();
+
+    // отображаем диалоговое окно для выбора времени
+    public void setTime(View v) {
+        new TimePickerDialog(MainActivity.this, t,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true)
+                .show();
     }
-    public void onNeutralClicked() {
-        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"На паузе\"!",
-                Toast.LENGTH_LONG).show();
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        }
+    };
+    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateAndTime.set(Calendar.MINUTE, minute);
+        }
+    };
+    public void progressDialog(View view){
+        final ProgressDialog dialog= ProgressDialog.show(this,"Мозг студента", "Думает",true);
+        dialog.show();
     }
 }
+
---
+++ dialog/src/main/java/com/mirea/schelukhinka/dialog/MyTimeDialogFragment.java
@@ -0,0 +1,13 @@
+package com.mirea.schelukhinka.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+public class MyTimeDialogFragment extends DialogFragment {
+
+}
